3ac328c4f024e642df5e34ee4d9a3259029c5aa5
lzy <lzy@7950x3d.com>
2023-12-28 18:15:35 +0800

    magnum示例代码： 1.已开放、测试通过：Triangle、Model Viewer、Area Lights、Audio、Box2D、2D Fluid Simulation、3D Fluid Simulation、Ray Tracing、Octree、ImGui、Animated GIF、Motion blur； 2.编译未通过的示例：Bullet Physics、DART Physics； 3.还有其他示例未测试，但估计问题不大；

---
+++ vcpkg_install.bat
@@ -0,0 +1,2 @@
+.\vcpkg\vcpkg.exe install --head corrade[*] magnum magnum-integration[bullet,eigen,glm] magnum-plugins[*] magnum-extras[bullet,eigen,glm]
+.\vcpkg\vcpkg install box2d
---
+++ vcpkg_install.sh
@@ -0,0 +1,2 @@
+.\vcpkg\vcpkg install --head corrade[*] magnum magnum-integration[bullet,eigen,glm] magnum-plugins[*] magnum-extras[bullet,eigen,glm]
+.\vcpkg\vcpkg install box2d
